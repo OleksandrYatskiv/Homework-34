ed3772b93e9a837e3d687feb51b90c48ab005ede
OleksandrYatskiv <yatsk1660@gmail.com>
2023-06-29 14:23:59 +0200

    Finished work

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    let sidebar = document.querySelector('#sidebar');
+    sidebar.style.display = "block";
+}
+
+function hideSidebar() {
+    let sidebar = document.querySelector('#sidebar');
+    sidebar.style.display = "none";
+}
